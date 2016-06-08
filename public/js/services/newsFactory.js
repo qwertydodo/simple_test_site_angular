cf170e63400db7b116085ae0790cb1db92a05458
PribyshPI <p.pribysh@itransition.com>
2016-06-08 12:22:06 +0300

    Refactoring

--- public/js/services/newsFactory.js
+++ public/js/services/newsFactory.js
@@ -1,29 +1,30 @@
 (function() {
 	var app = angular.module('app');
 	
-	app.factory('newsFactory', function($http, config){
-		var	URL = config.URL_SERVER + '/news/';
-    	
-    	return {
-        	getNews: function(data) {
-        		data = data || {};
+    function newsFactory($http, config) {
+        var URL = config.URL_SERVER + '/news/';
+        
+        return {
+            getNews: function(data) {
+                data = data || {};
+                return $http({
+                            url: URL + (data.id ? data.id : ''),
+                            method: 'GET',
+                            params: data.params
+                        });
+            },
+            updateNews: function(data) {
+                var url = config.URL_SERVER + '/news/';
 
-	           	return $http({
-            				url: URL + (data.id ? data.id : ''),
-            				method: 'GET',
-            				params: data.params
-            			});
-        	},
-        	updateNews: function(data) {
-        		var url = config.URL_SERVER + '/news/';
+                return $http({
+                            url: URL + (data.id ? data.id : ''),
+                            method: 'POST',
+                            data: data
+                        });
+            }
+        };
+    }
 
-        		return $http({
-            				url: URL + (data.id ? data.id : ''),
-            				method: 'POST',
-            				data: data
-            			});
-        	}
-    	};
-	});
+	app.factory('newsFactory', newsFactory);
 
 })();
