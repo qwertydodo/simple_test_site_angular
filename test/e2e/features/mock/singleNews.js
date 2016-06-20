c8195fb2d137b506b5c67f5b9f6d0acde5cc0247
PribyshPI <p.pribysh@itransition.com>
2016-06-20 16:44:09 +0300

    Mock for the single news object

---
+++ test/e2e/features/mock/singleNews.js
@@ -0,0 +1,7 @@
+module.exports = {
+	header: {val: 'testHeader', type: 'input'},
+	category: {val: 'Music', type: 'select'},
+	dt: {val: '01/01/2001', type: 'input'},
+	prevtext: {val: 'testPrev', type: 'input'},
+	text: {val: 'testText', type: 'textarea'}
+};
